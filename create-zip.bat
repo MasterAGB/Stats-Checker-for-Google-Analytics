f4d6634d580112b842c3dfd5240bcf5a5e61e8ff
Andrejs Birzgals <andrejs.birzgals@gmail.com>
2013-05-21 18:04:13 +0300

    BAT file for generating zip

---
+++ create-zip.bat
@@ -0,0 +1,9 @@
+SET WINRAR="C:\Program Files\WinRAR"
+CD "C:\Users\AGB\Dropbox\Google chrome extensions\APPS\Google Analytics\"
+%WINRAR%\WinRAR.exe a "ext.zip" *.html *.css *.json img js _locales
+
+
+
+
+
+
